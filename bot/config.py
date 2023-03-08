840f0ec66f334d7c96d51338b6b686d342148f3f
vkingw <alfa.king+git@gmail.com>
2023-03-08 23:19:10 +0800

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -18,5 +18,3 @@ use_chatgpt_api = config_yaml.get("use_chatgpt_api", True)
 allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
 mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
-#mongodb_uri = "mongodb://{config_env['MONGODB_USER']}:{config_env['MONGODB_PASSWORD']}@{config_env['MONGODB_URL']}/"
-mongodb_uri = "mongodb://{config_env['MONGODB_URL']}/"
